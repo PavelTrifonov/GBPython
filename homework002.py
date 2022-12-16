755886d61444fe2bfe266e48f91cfadbad4a50c8
Pavel <pashok_163@mail.ru>
2022-12-16 09:59:56 +0300

    Решили 2 задачу из первого семинара

--- homework002.py
+++ homework002.py
@@ -1,2 +1,11 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z 
 # для всех значений предикат.
+
+X=(True,False)
+Y=(True,False)
+Z=(True,False)
+for i in X:
+    for j in Y:
+        for k in Z:
+            print(f'¬({X[i]} ⋁ {Y[j]} ⋁ {Z[k]}) = ¬{X[i]} ⋀ ¬{Y[j]} ⋀ ¬{Z[k]}' )
+            print(not (X[i] or Y[j] or Z[k])== (not X[i]) and (not Y[j]) and (not Z[k]))
